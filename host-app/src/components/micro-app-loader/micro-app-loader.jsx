56e94b871da02aea91b955a6c2a07cc458613216
Chris Galvan <chrisgalvanr@gmail.com>
2019-07-05 13:41:40 -0500

    changes to host
    The micro-app-loader now looks for the asset-manifest.json file to load the latest version of the external bundle.

--- host-app/src/components/micro-app-loader/micro-app-loader.jsx
+++ host-app/src/components/micro-app-loader/micro-app-loader.jsx
@@ -1,93 +1,100 @@
-import React, {PureComponent} from 'react';
-import Script from 'react-load-script'
+import React, { Component } from 'react';
+import Script from 'react-load-script';
+
+const getScriptUrl = async (url, entryFile) => {
+    const manifestInfo = await fetch(url);
+    const json = await manifestInfo.json();
+
+    return json[entryFile];
+}
 
 /**
  * The MicroAppLoader component is responsible for injecting a
  * micro-app and its bundle. It also insures the bundle is only 
  * loaded once if the micro-app is referenced in the host multiple times.
  */
-class MicroAppLoader extends PureComponent {
+class MicroAppLoader extends Component {
 
-    // Create a placeholder to mount and unmount the micro app.
-    placeholder = React.createRef();
+    constructor(props) {
+        super(props);
+        this.placeholder = React.createRef();
+        this.state = {
+            scriptUrl: '',
+            loaded: false,
+            mounted: false,
+        };
+
+        this.handleScriptCreate = this.handleScriptCreate.bind(this);
+        this.handleScriptError = this.handleScriptError.bind(this);
+        this.handleScriptLoad = this.handleScriptLoad.bind(this);
+    }
 
     componentDidMount() {
-        
         // Check to see if bundle for this micro-app has already been injected.
-        if(window.loadedScripts !== undefined) {
+        const { plugin } = this.props.config;
+        const manifestUrl = `${plugin.path}${plugin.manifest}`;
 
-            if(window[this.props.config.plugin.name] !== undefined && window.loadedScripts.indexOf(this.props.config.plugin.path) > -1) {
-
-                // Render the micro app with props.
-                window[this.props.config.plugin.name].mount(this.props, this.placeholder.current);
-            }
-        }
+        getScriptUrl(manifestUrl, plugin.entry).then(fileName => {
+            this.setState({scriptUrl: `${plugin.path}${fileName}`});
+        });
     }
 
     componentDidUpdate(prevProps) {
+        if (!this.state.mounted && this.state.loaded) {
+            // mount the microapp
+            window[this.props.config.plugin.name].mount(this.props, this.placeholder.current);
+            this.setState({ mounted: true });
+        }
 
-        if(prevProps !== this.props){
-
+        if(this.state.mounted && prevProps !== this.props) {
             // Update the micro app with new props.
             window[this.props.config.plugin.name].mount(this.props, this.placeholder.current);
         }
     }
 
     componentWillUnmount(){
-
         // Destroy the micro app.
         window[this.props.config.plugin.name].unmount(this.placeholder.current);
     }
 
     handleScriptCreate() {
-        console.log(`script ${this.props.config.plugin.path} created`)
-      }
+        console.log(`script ${this.props.config.plugin.path} created`);
+    }
 
-      handleScriptError() {
-        console.log(`script ${this.props.config.plugin.path} failed to load`)
-      }
+    handleScriptError() {
+        console.log(`script ${this.props.config.plugin.path} failed to load`);
+    }   
        
-      handleScriptLoad() {
+    handleScriptLoad() {
+        this.setState({ loaded: true });
 
-        // Verify script collection has been instantiated.
-        if (window.loadedScripts === undefined) {
-            window.loadedScripts = [];
-        }
-
-        // Add the new script to the collection.
-        window.loadedScripts.push(this.props.config.plugin);
-
-        console.log(`script ${this.props.config.plugin.path} loaded`)
+        console.log(`script ${this.props.config.plugin.path} loaded`);
 
         if (window[this.props.config.plugin.name] !== undefined) {
-
             // Render the micro app with props.
             window[this.props.config.plugin.name].mount(this.props, this.placeholder.current);
         } else {
-
             throw Error(`Unable to mount micro-app ${this.props.config.plugin.name}, window.${this.props.config.plugin.name} is not defined`)
         }
-      }
+    }
 
     render() {
 
-        let script;
+        let script = null;
+        const { loaded, scriptUrl } = this.state;
 
-        // Inject the bundle with a script tag.
-        if(window.loadedScripts === undefined || window.loadedScripts.indexOf(this.props.config.plugin.path) === -1) {
-            script = <Script url={this.props.config.plugin.path}
-                             onCreate={this.handleScriptCreate.bind(this)}
-                             onError={this.handleScriptError.bind(this)}
-                             onLoad={this.handleScriptLoad.bind(this)} />
-        } else{
-
-            script = null;
-        }
-
-        return <div style={{width: '100%'}}>
-                    {script}
-                    <div style={{height: '100%'}} ref={this.placeholder}></div>
-                </div>
+        return  (
+            <div style={{width: '100%'}}>
+                {script}
+                {!loaded && scriptUrl && (
+                    <Script 
+                        url={scriptUrl}
+                        onCreate={this.handleScriptCreate}
+                        onError={this.handleScriptError}
+                        onLoad={this.handleScriptLoad} />
+                )}
+                <div style={{height: '100%'}} ref={this.placeholder}></div>
+            </div>)
     }
 }
 
--- host-app/src/configurations/site/redbox.js
+++ host-app/src/configurations/site/redbox.js
@@ -12,7 +12,10 @@ export const config = {
                     alias: 'movie-list',
                     plugin: {
                         name: 'MovieSearchApp',
-                        path: '/micro-apps/redbox/search/static/js/redbox-search-micro-app.js',
+                        //path: 'https://micro-app-marketplace.netlify.com/content/micro-apps/search/redbox-search-micro-app.9ce825cf8e68ac5ceaee.js',
+                        path: 'https://micro-app-marketplace.netlify.com/content/micro-apps/search/',
+                        manifest: 'asset-manifest.json',
+                        entry: 'main.js',
                         isMicroApp: true,
                         props: {
 
@@ -23,7 +26,10 @@ export const config = {
                     alias: 'movie-details',
                     plugin: {
                         name: 'MovieDetailsApp',
-                        path: '/micro-apps/redbox/details/static/js/redbox-details-micro-app.js',
+                        // path: 'https://micro-app-marketplace.netlify.com/content/micro-apps/details/redbox-details-micro-app.d8ef60fae4c8e4b1ab4f.js',
+                        path: 'https://micro-app-marketplace.netlify.com/content/micro-apps/details/',
+                        manifest: 'asset-manifest.json',
+                        entry: 'main.js',
                         isMicroApp: true,
                         props: {
 
